2ef3d3244f193387ba862807add582641b79e1d5
Sergey Tsimfer <Tsimfer.SA@gazprom-neft.ru>
2023-02-20 13:35:58 +0300

    Update notebook images

--- segfast/__init__.py
+++ segfast/__init__.py
@@ -2,3 +2,5 @@
 from .segyio_loader import SegyioLoader, SafeSegyioLoader
 from .memmap_loader import MemmapLoader
 from .loader import Loader, File, open #pylint: disable=redefined-builtin
+
+__version__ = '0.9.0'
