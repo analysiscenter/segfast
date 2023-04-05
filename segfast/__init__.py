7af0c9bd89d2b2294de6c01b003d0233660f8bd7
SergeyTsimfer <47103382+SergeyTsimfer@users.noreply.github.com>
2023-04-05 14:20:53 +0300

    Add release action (#2)

--- segfast/__init__.py
+++ segfast/__init__.py
@@ -3,4 +3,4 @@ from .segyio_loader import SegyioLoader, SafeSegyioLoader
 from .memmap_loader import MemmapLoader
 from .loader import Loader, File, open #pylint: disable=redefined-builtin
 
-__version__ = '0.9.1'
+__version__ = '1.0.0'
